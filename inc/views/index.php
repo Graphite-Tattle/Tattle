3a2e91f5b39ada99625842cd58e1394f09a188b5
Chris McCoy <chris@brownbagcode.com>
2012-12-12 11:42:22 -0500

    fixing issue #29

--- inc/views/index.php
+++ inc/views/index.php
@@ -3,27 +3,31 @@ $tmpl->set('title', 'Self Service Alerts based on Graphite metrics');
 $tmpl->set('breadcrumbs',$breadcrumbs);
 $tmpl->place('header');
 try {
-        $results->tossIfNoRows();
-	?>
+        if (method_exists($results, 'tossIfNoRows')) {
+            $results->tossIfNoRows();
+        } else {
+            throw new fNoRowsException();
+        }
+?>
   <table class="zebra-striped">
     <thead>
-    <tr>    
+    <tr>
     <th>Check</th>
     <th>Latest Status</th>
     <th>Last Alert</th>
     <th>Alert Count</th>
     <th>Action</th>
-       </tr></thead><tbody>    
-	<?php
-	$first = TRUE;
-	foreach ($results as $row) {
+       </tr></thead><tbody>
+<?php
+        $first = TRUE;
+        foreach ($results as $row) {
           $check = new Check($row['check_id']);
-		?>
-    	<tr>
+?>
+        <tr>
         <td><?=$row['name']; ?></td>
         <td><?=$status_array[$row['status']]; ?></td>
         <td><?=$row['timestamp']; ?></td>
-	<td><?=$row['count']; ?></td>
+        <td><?=$row['count']; ?></td>
         <td><a href="<?=CheckResult::makeURL('list', $check); ?>">View</a> | <a href="<?=CheckResult::makeURL('ackAll', $check); ?>">Clear</a>
         </td>
         </tr>
@@ -31,7 +35,7 @@ try {
     //check to see if paging is needed
     $total_pages = ceil($alert_count / $GLOBALS['PAGE_SIZE']);
     if ($total_pages > 1) {
-      $prev_class = 'previous'; 
+      $prev_class = 'previous';
       $prev_link = fURL::get() . '?page=' . ($page_num - 1);
       $next_class = 'next';
       $next_link = fURL::get() . '?page=' . ($page_num + 1);
@@ -56,10 +60,10 @@ try {
     </div>
     <? }
 } catch (fNoRowsException $e) {
-	?>
-	<p class="info">There are currently no Alerts based on your subscriptions. Smile, looks like everything is happy!</p>
+?>
+        <p class="info">There are currently no Alerts based on your subscriptions. Smile, looks like everything is happy!</p>
         <p class="warn">This could also mean that you haven't set up the cronjob to poll the processor? <a href="processor.php">Processor</a></p>
-	<?php
+<?php
 }
 ?>
 </div>
--- index.php
+++ index.php
@@ -7,34 +7,21 @@ $breadcrumbs[] = array('name' => 'Alerts', 'url' => 'index.php','active' => fals
 $page_num = fRequest::get('page', 'int', 1);
 $offset = ($page_num - 1)*$GLOBALS['PAGE_SIZE'];
 
-$alert_count_query = 'SELECT count(distinct c.check_id) as count '.
-                 'FROM subscriptions s '.
-                 'JOIN checks c ON s.check_id = c.check_id '.
-                 'JOIN check_results r ON s.check_id = r.check_id '.
-                 'WHERE r.timestamp >= DATE_SUB(CURDATE(),INTERVAL 1 DAY) '.
-                 'AND r.status IS NOT NULL '.
-                 'AND acknowledged = 0 '.
-                 'AND s.user_id = ' . fSession::get('user_id') . ';';
-
-$alert_count_results = $db->query($alert_count_query);
-$alert_count = $alert_count_results->fetchScalar();
 $results = NULL;
-if ($alert_count > $GLOBALS['PAGE_SIZE']) {
 
-    //We need to get both the current page and a count to determine whether paging is needed.
-    $latest_alerts = 'SELECT c.check_id,name,r.status,count(c.check_id) as count, r.timestamp '.
-                     'FROM subscriptions s '.
-                     'JOIN checks c ON s.check_id = c.check_id '.
-                     'JOIN check_results r ON s.check_id = r.check_id '.
-                     'WHERE r.timestamp >= DATE_SUB(CURDATE(),INTERVAL 1 DAY) '.
-                     'AND r.status IS NOT NULL '.
-                     'AND acknowledged = 0 '.
-                     'AND s.user_id = ' . fSession::get('user_id') . ' ' .
-                     'GROUP BY c.check_id ' .
-                     'LIMIT ' . $GLOBALS['PAGE_SIZE'] . ' ' .
-                     'OFFSET ' . $offset . ';';
+$latest_alerts = 'SELECT c.check_id,name,r.status,count(c.check_id) as count, r.timestamp '.
+                    'FROM subscriptions s '.
+                    'JOIN checks c ON s.check_id = c.check_id '.
+                    'JOIN check_results r ON s.check_id = r.check_id '.
+                    'WHERE r.timestamp >= DATE_SUB(CURDATE(),INTERVAL 1 DAY) '.
+                    'AND r.status IS NOT NULL '.
+                    'AND acknowledged = 0 '.
+                    'AND s.user_id = ' . fSession::get('user_id') . ' ' .
+                    'GROUP BY c.check_id ' .
+                    'LIMIT ' . $GLOBALS['PAGE_SIZE'] . ' ' .
+                    'OFFSET ' . $offset . ';';
 
-    $results = $db->query($latest_alerts);
-}
+$results = $db->query($latest_alerts);
+$alert_count = $results->countReturnedRows();
 
 include 'inc/views/index.php';

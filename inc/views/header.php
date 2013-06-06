85a668c0b53d7e1413727a16e04b926c018f5190
Florent Couffe <florent.couffe.prestataire@sfr.com>
2013-06-06 18:32:26 +0200

    Move the function "is_menu_active"
    It makes more sense to have this function in fURL than in fActiveRecord

--- inc/views/header.php
+++ inc/views/header.php
@@ -86,24 +86,24 @@ if (!$this->get('full_screen')) { ?>
 	            <?
 	
 	              $current_url = '?'.fURL::getQueryString();
-	              echo '<li' . (fActiveRecord::is_menu_active('index') ? ' class="active"' : '') . '><a href="index.php">Alerts</a></li>'. "\n";
+	              echo '<li' . (fURL::is_menu_active('index') ? ' class="active"' : '') . '><a href="index.php">Alerts</a></li>'. "\n";
 	              $threshold_check_list = Check::makeURL('list', 'threshold');
-	              echo '<li' . (fActiveRecord::is_menu_active('check','type=threshold') ? ' class="active"' : '') . '><a href="' . $threshold_check_list . '" >Threshold Checks</a></li>' . "\n";
+	              echo '<li' . (fURL::is_menu_active('check','type=threshold') ? ' class="active"' : '') . '><a href="' . $threshold_check_list . '" >Threshold Checks</a></li>' . "\n";
 	              $predictive_check_list = Check::makeURL('list', 'predictive');
-	              echo '<li' . (fActiveRecord::is_menu_active('check','type=predictive') ? ' class="active"' : '') . '><a href="' . $predictive_check_list . '" >Predictive Checks</a></li>' . "\n";
+	              echo '<li' . (fURL::is_menu_active('check','type=predictive') ? ' class="active"' : '') . '><a href="' . $predictive_check_list . '" >Predictive Checks</a></li>' . "\n";
 	              $subscription_list = Subscription::makeURL('list');
-	              echo '<li' . (fActiveRecord::is_menu_active('subscription') ? ' class="active"' : '') .'><a href="' . $subscription_list . '" >Subscriptions</a></li>' . "\n";
+	              echo '<li' . (fURL::is_menu_active('subscription') ? ' class="active"' : '') .'><a href="' . $subscription_list . '" >Subscriptions</a></li>' . "\n";
 	              $dashboard_list = Dashboard::makeURL('list');
-	              echo '<li' . (fActiveRecord::is_menu_active('dashboard') ? ' class="active"' : '') . '><a href="' . $dashboard_list . '">Dashboards</a></li>';
+	              echo '<li' . (fURL::is_menu_active('dashboard') ? ' class="active"' : '') . '><a href="' . $dashboard_list . '">Dashboards</a></li>';
 	              $group_list = Group::makeURL('list');
-	              echo '<li' . (fActiveRecord::is_menu_active('group') ? ' class="active"' : '') . '><a href="' . $group_list . '">Groups</a></li>';
+	              echo '<li' . (fURL::is_menu_active('group') ? ' class="active"' : '') . '><a href="' . $group_list . '">Groups</a></li>';
 	              $setting_list = Setting::makeURL('list','user');
-	              echo '<li' . (fActiveRecord::is_menu_active('setting','type=user') ? ' class="active"' : '') . '><a href="' . $setting_list . '" >Settings</a></li>' . "\n";
+	              echo '<li' . (fURL::is_menu_active('setting','type=user') ? ' class="active"' : '') . '><a href="' . $setting_list . '" >Settings</a></li>' . "\n";
 	if (fAuthorization::checkAuthLevel('admin')) {
 	              $setting_list = Setting::makeURL('list','system');
-	              echo '<li' . (fActiveRecord::is_menu_active('setting','type=system') ? ' class="active"' : '') . '><a href="' . $setting_list . '" >System Settings</a></li>' . "\n";
+	              echo '<li' . (fURL::is_menu_active('setting','type=system') ? ' class="active"' : '') . '><a href="' . $setting_list . '" >System Settings</a></li>' . "\n";
 	              $user_list = User::makeURL('list');
-	              echo '<li' . (fActiveRecord::is_menu_active('user') ? ' class="active"' : '') . '><a href="' . User::makeURL('list') . '" >Users</a></li>';
+	              echo '<li' . (fURL::is_menu_active('user') ? ' class="active"' : '') . '><a href="' . User::makeURL('list') . '" >Users</a></li>';
 	}
 	?>
 	          </ul>

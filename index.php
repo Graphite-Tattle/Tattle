b866798982369e06fb4868c01e28a44d7f1ac05d
Matthew Barlocker <matthew@lucidchart.com>
2013-07-02 16:01:38 -0600

    get correct count for alerts - makes pagination work on homepage

--- index.php
+++ index.php
@@ -22,6 +22,16 @@ $latest_alerts = 'SELECT c.check_id,name,r.status,count(c.check_id) as count, r.
                     'OFFSET ' . $offset . ';';
 
 $results = $db->query($latest_alerts);
-$alert_count = $results->countReturnedRows();
+
+$alert_count_query = 'SELECT COUNT(1) as count '.
+                    'FROM subscriptions s '.
+                    'INNER JOIN checks c ON s.check_id = c.check_id '.
+                    'JOIN check_results r ON s.check_id = r.check_id '.
+                    'WHERE r.timestamp >= DATE_SUB(CURDATE(),INTERVAL 1 DAY) '.
+                    'AND r.status IS NOT NULL '.
+                    'AND acknowledged = 0 '.
+                    'AND s.user_id = ' . fSession::get('user_id') . ' ' .
+                    'GROUP BY c.check_id;';
+$alert_count = $db->query($alert_count_query)->countReturnedRows();
 
 include 'inc/views/index.php';

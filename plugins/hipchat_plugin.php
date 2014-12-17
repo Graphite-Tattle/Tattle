ecd7c82f012f59df024e8379ff4c5d497fe9b543
Ed McLain <emclain@digitalmotion.co>
2014-12-17 14:24:32 -0600

    Fixed debug code for hipchat

--- plugins/hipchat_plugin.php
+++ plugins/hipchat_plugin.php
@@ -30,71 +30,56 @@ function hipchat_send_methods() {
       return array('hipchat_notify' => 'HipChat');
 }
 
-function hipchat_notify($check, $check_result) {
-    	
+function hipchat_notify($check, $check_result, $subscription)) {
     global $status_array;
-    global $debug;
-	
+	  global $debug;
+
     $state = $status_array[$check_result->getStatus()];
-	
+
     if (strtolower($state) == 'ok') {
-    	
         $color = sys_var('hipchat_ok_color');
-		
     } elseif (strtolower($state) == 'warning') {
-    	
         $color = sys_var('hipchat_warning_color');
-		
     } elseif (strtolower($state) == 'error') {
-    	
         $color = sys_var('hipchat_error_color');
-		
     }
 
-	$url = $GLOBALS['TATTLE_DOMAIN'] . '/' . CheckResult::makeURL('list',$check_result);
+    $url = $GLOBALS['TATTLE_DOMAIN'] . '/' . CheckResult::makeURL('list',$check_result);
 
     $data = array(
         'color' => $color,
         'notify' =>  sys_var('hipchat_notify'),
         'message_format' => 'html',
         'message' => "<b>" . $check->prepareName() . "</b><br />The check returned {$check_result->prepareValue()}<br />View Alert Details : <a href=\"" . $url . "\">" . $url . "</a>"
-	);
-	
-	if ($debug) {
-		
-		$url = 'https://api.hipchat.com/v2/room?auth_token=' . sys_var('hipchat_apikey');
-		$c = curl_init();
-		curl_setopt($c, CURLOPT_URL, $url);
-		curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-		
-		echo "\n\nRooms: " . curl_exec($c) . "\n\n";
-		echo "\n\nURL: " . 'https://api.hipchat.com/v2/room/' . strtolower(sys_var('hipchat_room')) . '/notification?auth_token=' . sys_var('hipchat_apikey') . "\n\n";
-		echo "\n\nData: " . print_r($data, true) . "\n\n";
-		
-	}
+	  );
 
-	$url = 'https://api.hipchat.com/v2/room/' . strtolower(sys_var('hipchat_room')) . '/notification?auth_token=' . sys_var('hipchat_apikey');
-	$c = curl_init();
-	curl_setopt($c, CURLOPT_URL, $url);
-	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($c, CURLOPT_HTTPHEADER, array(                                                                          
-    	'Content-Type: application/json',                                                                                
-    	'Content-Length: ' . strlen(json_encode($data)))
-	);
-	curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($data));
+    if ($debug) {
+			$url = 'https://api.hipchat.com/v2/room?auth_token=' . sys_var('hipchat_apikey');
+		  $c = curl_init();
+		  curl_setopt($c, CURLOPT_URL, $url);
+		  curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
 
-	$response = curl_exec($c);
-	
-	if ($response === false) {
-		
-    	echo "\n\nCurl error: " . curl_error($c) . "\n\n";
-		
-	}
-	
-	if ($debug) {
-		
-		echo "\n\nResponse: " . curl_getinfo($c, CURLINFO_HTTP_CODE)  . ' - ' . $response . "\n\n";
-		
-	}
-	
+		  fCore::debug("Rooms: " . curl_exec($c) . "\n", FALSE);
+		  fCore::debug("URL: " . 'https://api.hipchat.com/v2/room/' . strtolower(sys_var('hipchat_room')) . '/notification?auth_token=' . sys_var('hipchat_apikey') . "\n", FALSE);
+		  fCore::debug("Data: " . print_r($data, true) . "\n", FALSE);
+    }
+
+    $url = 'https://api.hipchat.com/v2/room/' . strtolower(sys_var('hipchat_room')) . '/notification?auth_token=' . sys_var('hipchat_apikey');
+    fCore::debug("HipChat Calling: $url", FALSE);
+  	$c = curl_init();
+  	curl_setopt($c, CURLOPT_URL, $url);
+  	curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+  	curl_setopt($c, CURLOPT_HTTPHEADER, array(
+      	'Content-Type: application/json',
+      	'Content-Length: ' . strlen(json_encode($data)))
+  	);
+  	curl_setopt($c, CURLOPT_POSTFIELDS, json_encode($data));
+
+  	$response = curl_exec($c);
+
+  	if ($response === false) {
+      	fCore::debug("Curl error: " . curl_error($c) . "\n", FALSE);
+  	}
+
+    echo "\n\nResponse: " . curl_getinfo($c, CURLINFO_HTTP_CODE)  . ' - ' . $response . "\n\n";
 }

9e850c9f1bb4381c76aff622cc44c50cc553013a
Grégoire Barbier <devel@g76r.eu>
2016-02-16 17:07:53 +0100

    added sql upgrade code for groups table
    it was lacking for a long time and is needed to have a consistent
    v1.0.0 to v1.1.0 sql upgade script

--- graphite_tattle_schema_upgrade_v1.0.0_to_v1.1.0.sql
+++ graphite_tattle_schema_upgrade_v1.0.0_to_v1.1.0.sql
@@ -13,3 +13,11 @@ ALTER TABLE graphs ADD COLUMN `starts_at_midnight` TINYINT(1) DEFAULT 0;
 
 ALTER TABLE checks MODIFY COLUMN last_check_value bigint;
 
+CREATE TABLE `groups` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `name` (`name`)
+) CHARSET=utf8;
+INSERT INTO groups VALUES (1, 'Default group', 'This group is the default group. It can\'t be deleted nor edited.');
